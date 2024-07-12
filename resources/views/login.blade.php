eb626483ba71addeb4db3b40f81a83361d1dc421
Rexx . <imatzzz2010@gmail.com>
2024-07-13 02:47:12 +0800

    fixing3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,57 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- FONT -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-      rel="stylesheet"
-    />
-    <!-- // -->
-    <link rel="stylesheet" href="/login/style.css" />
-    <link rel="stylesheet" href="/bootstrap-5.2.3-dist/css/bootstrap.min.css" />
-    <title>Login</title>
-  </head>
-  <body>
-    <div class="wrapper">
-      <div class="container main">
-        <div class="row">
-          <div class="col-md-6 side-image">
-            <!-- Image -->
-            <div class="text">
-              <p>Login aplikasi</p>
+<x-login-lay>
+    <div class="px-[50vw] py-[50vh] flex flex-row justify-center">
+        <div class="bg-slate-400 p-[10rem] flex-col">KIRI</div>
+        <div class="bg-slate-500 p-[10rem] flex-col rounded-lg">
+            <div class="text-center text-[2rem] font-bold pb-[4rem]">Create Account</div>
+
+            <div class="bg-slate-400 p-[3rem] flex flex-col justify-center rounded-lg">
+                <p class="text-left">Email</p>
+                <input class="rounded-lg h-[2rem] w-[19rem] text-center" type="text" id="email" required
+                    autocomplete="off">
+                <p class="text-left">Password</p>
+                <input class="rounded-lg h-[2rem] w-[19rem]" type="text" id="email" required autocomplete="off">
+                <div class="bg-slate-400 p-[1rem] text-center">SIGN UP</div>
             </div>
-          </div>
-          <div class="col-md-6 right">
-            <div class="input-box">
-              <header>Create Account</header>
-              <div class="input-field">
-                <input
-                  type="text"
-                  class="input"
-                  id="email"
-                  required
-                  autocomplete="off"
-                />
-                <label for="email">Email</label>
-              </div>
-              <div class="input-field">
-                <input type="password" class="input" id="password" required />
-                <label for="password">Password</label>
-              </div>
-              <div class="input-field">
-                <input type="submit" class="submit" value="Sign Up" />
-              </div>
-              <div class="signin">
-                <span>Already have account? <a href="#">Login Here</a></span>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
+
     </div>
-  </body>
-</html>
+
+</x-login-lay>
